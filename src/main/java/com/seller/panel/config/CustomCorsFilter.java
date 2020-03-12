265ef774918c7cda5719c2492a3ed21249b57416
Vishal Gupta <vishal.gupta@noosyntech.in>
2020-03-12 16:53:31 +0530

    Handle OPTIONS method

--- src/main/java/com/seller/panel/config/CustomCorsFilter.java
+++ src/main/java/com/seller/panel/config/CustomCorsFilter.java
@@ -1,16 +1,10 @@
 package com.seller.panel.config;
 
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
-
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CustomCorsFilter implements Filter {
 
     @Override
@@ -20,10 +14,14 @@ public class CustomCorsFilter implements Filter {
         final HttpServletRequest request = (HttpServletRequest) req;
         request.setAttribute("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, X-Requested-With, Accept");
         response.setHeader("Access-Control-Max-Age", "1800");
-        chain.doFilter(req, res);
+        if ("OPTIONS".equalsIgnoreCase(((HttpServletRequest) req).getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
     }
     @Override
     public void destroy() {

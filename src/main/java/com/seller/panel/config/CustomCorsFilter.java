fe24e359e5c330d1cf39380bed8e8cf18950c717
Vishal Gupta <vishal.gupta@noosyntech.in>
2020-03-12 16:54:50 +0530

    Add bymistake removed code

--- src/main/java/com/seller/panel/config/CustomCorsFilter.java
+++ src/main/java/com/seller/panel/config/CustomCorsFilter.java
@@ -1,10 +1,16 @@
 package com.seller.panel.config;
 
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CustomCorsFilter implements Filter {
 
     @Override
